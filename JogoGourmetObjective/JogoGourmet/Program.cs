ade9009fb4c3b86566edc154805f0f01cb9ad69b
Rafael Arthur Rocha Miranda <43150739+rafael-miranda10@users.noreply.github.com>
2020-09-01 11:58:49 -0300

    criando as entidades

---
+++ JogoGourmetObjective/JogoGourmet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JogoGourmet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
